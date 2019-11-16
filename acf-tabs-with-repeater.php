f0d8a4480e0ebcc5d098f8d1d501963bc340a2d5
stevelutz <stevenlutz27@gmail.com>
2019-11-15 21:44:59 -0800

    Update acf-tabs-with-repeater.php
    Adding in JS for tabs

--- acf-tabs-with-repeater.php
+++ acf-tabs-with-repeater.php
@@ -103,8 +103,40 @@
 </div>
 ​
 </div>
-Collapse
 
+<script>
+	
+//Tab Functionality
+$(document).ready(function() {
+
+    $('.nav-tabs li:first-child, .tab-content:first-child').addClass('active');
+    $('.tab-content:first-child').removeClass('hide');
+
+    $('.nav-tabs > li > a').click(function(event){
+    event.preventDefault();//stop browser
+
+    //get displaying tab content
+    var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');          
+
+    //find actived navigation and remove 'active' css
+    var actived_nav = $('.nav-tabs > li.active');
+    actived_nav.removeClass('active');
+
+    //add 'active' css into clicked navigation
+    $(this).parents('li').addClass('active');
+
+    //hide displaying tab content
+    $(active_tab_selector).removeClass('active');
+    $(active_tab_selector).addClass('hide');
+
+    //show target tab content
+    var target_tab_selector = $(this).attr('href');
+    $(target_tab_selector).removeClass('hide');
+    $(target_tab_selector).addClass('active');
+});
+});
+
+</script>
 
 
 
